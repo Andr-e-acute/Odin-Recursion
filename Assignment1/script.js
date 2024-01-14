8c173d74efb3202618bbd2d1ad0603b6b4a68297
André Potthast <andre.potthast.ap@googlemail.com>
2024-01-14 11:24:11 +0100

    Initial commit

---
+++ Assignment1/script.js
@@ -0,0 +1,5 @@
+// Using iteration, write a function fibs which takes a number and returns an
+// array containing that many numbers from the Fibonacci sequence.
+function fibs(num) {
+  console.log(arr);
+}
