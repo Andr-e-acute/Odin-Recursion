7a459c4475b76cf2a41240f76ded7d19a57785fe
André Potthast <andre.potthast.ap@googlemail.com>
2024-01-15 09:02:17 +0100

    initial commit start of pseudocode

---
+++ Assignment2/mergeSort.js
@@ -0,0 +1,25 @@
+// Assignment 2
+// Build a function mergeSort that takes in an array and returns a sorted array,
+// using a recursive merge sort methodology. \
+
+// Tips:
+// Think about what the base case is and what behavior is happening again and again\
+//  and can actually be delegated to someone else (e.g. that same function!).
+
+function mergeSort(arr) {
+  console.log(arr);
+  //base case single value return this value as Array
+  // split the array in half
+
+  // mergeSort each half
+
+  // when baseCase call merge.
+  // return single arr
+}
+
+function merge(leftArr, rightArr) {}
+
+// An input of
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+// should return [0, 1, 1, 2, 3, 5, 8, 13],
+// and an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
